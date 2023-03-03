1f79a189303dc3b89ab141bd27fa57130eb36242
pablocavaz04 <camvazpablo04@gmail.com>
2023-03-03 12:26:00 +0100

    Creacion Proyecto

---
+++ BuscaTesoro.java
@@ -0,0 +1,9 @@
+/**
+* Minijuego "Busca el tesoro"
+*
+* Se colocan una mina y un tesoro de forma aleatoria en un cuadrante de
+* cuatro filas por cinco columnas. El usuario intentará averiguar dónde
+* está el tesoro.
+*
+* @Pablo Camino Vázquez y Carlos Gil Sánchez
+*/
